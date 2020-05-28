6e15da6bf5ce777814266aaffd595ed2782e075f
Marcin Wijaszka <mwijaszka99@gmail.com>
2020-05-28 23:57:13 +0200

    Add include/*.h headers

---
+++ include/objetosc.h
@@ -0,0 +1,12 @@
+#ifndef OBJETOSC_H
+#define OBJETOSC_H
+
+
+class objetosc
+{
+    public:
+        void oblicz_objetosc(double a);
+        void oblicz_polec(double a);
+};
+
+#endif // OBJETOSC_H
---
+++ include/pole.h
@@ -0,0 +1,11 @@
+#ifndef POLE_H
+#define POLE_H
+
+
+class pole
+{
+    public:
+        void oblicz_pole(double a);
+};
+
+#endif // POLE_H
